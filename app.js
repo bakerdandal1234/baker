4e0fa5a551c67f94d21db7700f9430d2af221ccb
bakerdandal1234 <bakerdandal4@gmail.com>
2026-01-05 12:35:27 +0200

    Create app.js
    done

---
+++ app.js
@@ -0,0 +1,14 @@
+// localStorage
+let favorites = JSON.parse(localStorage.getItem('shortSentencesFavorites')) || [];
+let currentBtn = null;
+
+// الدوال هنا
+function toggleDarkMode() {
+    ...
+}
+
+function loadDarkMode() {
+    ...
+}
+
+// بقية الكود كله
